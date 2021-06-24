c33e16ae3e5b45028fd31328698b25d65fc3380c
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-06-24 16:28:26 -0300

    Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+import * as Yup from 'yup';
+import { Toast, Form, Button } from 'react-bootstrap';
 
 function App() {
+  const [nome, setNome] = useState();
+  const [enderco, setEnderco] = useState();
+  const [telefone, setTelefone] = useState();
+  const [email, setEmail] = useState();
+  const [dataDeNacismento, setDataDeNacismento] = useState();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+  
+  async function handleSubmit(e) {
+    e.preventDefault();
+    
+    const data = {
+      nome,
+      enderco,
+      telefone,
+      email,
+      dataDeNacismento
+    }
+
+    try {
+      const schema = Yup.object().shape({
+        nome: Yup.string().required("Nome é obrigatorio"),
+        enderco: Yup.string().required("Endereço obrigatorio"),
+        telefone: Yup.number().required("Telefone obrigatorio"),
+        password: Yup.string().min(6, "Senha tem que ter mais de 6 caracteres"),
+        email: Yup.string().email("Precisa ser um E-mail valido").required("E-mail é obrigatorio"),
+        dataDeNacismento: Yup.date().required("Data de nacismento obrigatoria")
+      })
+  
+      await schema.validate(data)
+
+      console.log(data);
+
+    } catch(err) {
+      setError(true);
+      setErrorMessage(err.message);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Form className="container">
+        {error && (
+          <Toast className="toast m-auto border border-danger" onClose={() => setError(false)}>
+            <Toast.Header>
+              <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+              <strong className="mr-auto text-danger">Error</strong>
+            </Toast.Header>
+            <Toast.Body>{errorMessage}</Toast.Body>
+          </Toast>
+        )}
+        
+        <Form.Group>
+          <Form.Label>Nome</Form.Label>
+          <Form.Control 
+            size="lg" 
+            type="text" 
+            placeholder="Digite seu nome" 
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mt-2">
+          <Form.Label>Endereço</Form.Label>
+          <Form.Control 
+            size="lg" 
+            type="text" 
+            placeholder="Digite seu endereço" 
+            value={enderco}
+            onChange={(e) => setEnderco(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group className="mt-2">
+          <Form.Label>Telefone</Form.Label>
+          <Form.Control 
+            size="lg" 
+            type="text"
+            placeholder="Digite seu telefone" 
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mt-2" controlId="formBasicEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control 
+            size="lg" 
+            type="email" 
+            placeholder="Digite sua senha" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mt-2">
+          <Form.Label>Data de nascimento</Form.Label>
+          <Form.Control 
+            size="lg" 
+            type="date" 
+            placeholder="Digite sua data de nascimento" 
+            value={dataDeNacismento}
+            onChange={(e) => setDataDeNacismento(e.target.value)}
+          />
+        </Form.Group>
+        <Button onClick={handleSubmit} className="mt-5 w-100" size="lg" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
   );
 }
 
